7f89b079e82b3b0f3bd9951cf0a2ea84b0faf938
Yury Chernyavski <yurachernyavskiw@gmail.com>
2023-09-07 18:43:39 +0200

    made a logo as a link to the main page

--- components/common/Siderbar/Sidebar.tsx
+++ components/common/Siderbar/Sidebar.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import Logo from "../assets/Logo_OWL.svg";
 import style from "./Sidebar.module.css";
 import { Menu } from "@/components/common";
+import Link from "next/link";
 
 export const Sidebar = () => {
   return (
     <aside className={style.sidebar}>
-      <Logo className={style.logo}/>
+      <Link href="/">
+        <Logo className={style.logo}/>
+      </Link>
       <div>Search</div>
       <Menu/>
     </aside>
