2ffb8847ea7c1a2c66bd4bd118672da42041e2cc
Yury Chernyavski <yurachernyavskiw@gmail.com>
2023-09-07 16:14:23 +0200

    Create a search component

--- components/common/Siderbar/Sidebar.tsx
+++ components/common/Siderbar/Sidebar.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import Logo from "../assets/Logo_OWL.svg";
 import style from "./Sidebar.module.css";
 import { Menu } from "@/components/common";
+import { Search } from "@/components/Search/Search";
 
 export const Sidebar = () => {
   return (
     <aside className={style.sidebar}>
       <Logo className={style.logo}/>
-      <div>Search</div>
-      <Menu/>
-    </aside>
+      <Search />
+      <Menu />
+    </aside >
   );
 };
