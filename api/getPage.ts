bd4fa29c746d8a1c40f04c30ccf421d3d1c3fbed
Yury Chernyavski <yurachernyavskiw@gmail.com>
2023-10-04 21:00:58 +0200

    changing

--- api/getPage.ts
+++ api/getPage.ts
@@ -3,5 +3,6 @@ import { API } from "@/helpers/api";
 
 export const getPage = async (alias: string): Promise<IPage> => {
   const res = await fetch( API.topPage.byAlias + alias);
+
   return res.json();
 };
